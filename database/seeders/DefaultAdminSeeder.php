0232d34db15b6b2dab51eb57e93f7c191ad16f84
NafiurSiddiqui <saejim91@gmail.com>
2024-04-15 22:23:13 +0600

    App: default Admin seeder created

---
+++ database/seeders/DefaultAdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DefaultAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin.bloggy@example.com',
+            'password' => bcrypt('secret'),
+            'role' => 'admin',
+        ]);
+
+    }
+}
