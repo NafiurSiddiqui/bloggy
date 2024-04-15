199ba7c9ede9f82767f76cbf0ff31ddad6f508c5
NafiurSiddiqui <saejim91@gmail.com>
2024-04-15 22:23:40 +0600

    UI: default index route for all posts created

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
