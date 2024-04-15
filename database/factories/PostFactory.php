0a8f636535945042ce3fbd50fda8aff0ecab943b
NafiurSiddiqui <saejim91@gmail.com>
2024-04-15 22:22:35 +0600

    DB: Post relationship, factory created

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory([
+                'role'=>'editor'
+            ]),
+            'title' => $this->faker->sentence,
+            'slug' => $this->faker->unique()->slug,
+            'excerpt' => $this->faker->text,
+            'body' => $this->faker->paragraph,
+            'thumbnail' => $this->faker->imageUrl(),
+            'published_at' => $this->faker->dateTime(),
+            'draft' => $this->faker->boolean,
+            'is_featured' => $this->faker->boolean,
+            'is_hot' => $this->faker->boolean,
+        ];
+    }
+}
---
+++ database/migrations/2024_04_15_125653_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+//            $table->foreignId('category_id');
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt');
+            $table->text('body');
+            $table->string('thumbnail')->nullable();
+            $table->timestamp('published_at')->nullable();
+            //draft state
+            $table->boolean('draft')->default(false);
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_hot')->default(false);
+            //meta stuff
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
