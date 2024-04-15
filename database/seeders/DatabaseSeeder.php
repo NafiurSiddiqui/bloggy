294a1eece451a308e865270ff6ba2f15d6f91a4c
NafiurSiddiqui <saejim91@gmail.com>
2024-04-15 18:53:53 +0600

    auth: default admin seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin.bloggy@example.com',
+            'password' => bcrypt('secret'),
+            'role' => 'admin',
         ]);
+
     }
 }
