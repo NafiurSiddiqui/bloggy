309b41287cc98be2378951d16da270ce371bd718
NafiurSiddiqui <saejim91@gmail.com>
2024-04-23 04:08:13 +0600

    UI: dark mode support added

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,7 +1,7 @@
 @props(['post'])
 
 <article
-    class="transition-colors duration-300 hover:bg-gray-100   rounded-xl borderTest "
+    class="transition-colors duration-300 hover:bg-gray-100  dark:hover:bg-gray-900 rounded-xl borderTest "
 >
     <div class="py-6 px-5">
         <div>
@@ -55,7 +55,7 @@
 
                 <div>
                     <a href="/posts/{{ $post->slug }}"
-                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
+                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8 dark:bg-gray-800 dark:hover:bg-gray-700">Read
                         More</a>
                 </div>
             </footer>
