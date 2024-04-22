3c9211c663c9196b1fd415b8f88242bdadd6a6d8
NafiurSiddiqui <saejim91@gmail.com>
2024-04-23 04:08:33 +0600

    UI (dashboard): wip

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,26 +4,29 @@
         Dashboard
     </x-slot:title>
 
-{{--    <x-slot name="header">--}}
-{{--        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">--}}
-{{--            {{ __('Dashboard') }}--}}
-{{--        </h2>--}}
-{{--    </x-slot>--}}
+    {{--    <x-slot name="header">--}}
+    {{--        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">--}}
+    {{--            {{ __('Dashboard') }}--}}
+    {{--        </h2>--}}
+    {{--    </x-slot>--}}
 
     <div class="flex min-h-screen ">
         <aside class="flex flex-col w-64 bg-gray-800 dark:bg-gray-900 text-white px-4 py-6">
             <ul class="space-y-2">
-                <x-dashboard.nav-link route="dashboard">
+                <x-dashboard.nav-link route="admin" :active="request()->is('admin')">
                     Dashboard
                 </x-dashboard.nav-link>
-                    <li>
-                    <a href="#" class="hover:text-gray-400 focus:outline-none focus:text-gray-300 p-2">Settings</a>
-                </li>
+
+                <x-dashboard.nav-link route="admin/create" :active="request()->is('admin/create')">
+                    Create post
+                </x-dashboard.nav-link>
+
             </ul>
         </aside>
         <section class="flex-grow p-4">
-            <h1>Welcome to your Dashboard!</h1>
-            <p>This is where you'll see all your important data and controls.</p>
+            @if(request()->is('dashboard/create-post'))
+                @include('admin.create')
+            @endif
         </section>
     </div>
 
