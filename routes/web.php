64e16ba98ad3601745a0419bcb016b2815139870
NafiurSiddiqui <saejim91@gmail.com>
2024-04-23 04:03:05 +0600

    Route: dynamic URL conflict resolved

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('category');
-Route::get('{category:slug}/{subcategory:slug}', [SubcategoryController::class, 'show'])->name('subcategory');
+Route::get('/categories/{category:slug}/{subcategory:slug}', [SubcategoryController::class, 'show'])->name('subcategory');
+
+
 
 
 //Route::get('/admin', function () {
@@ -28,10 +30,10 @@ Route::get('{category:slug}/{subcategory:slug}', [SubcategoryController::class, 
 //you have user (admin) side.
 
 Route::middleware('auth')->group(function () {
-//    Route::get('/dashboard', function () {
-//        return view('admin.dashboard');
-//    })->name('dashboard');
-    Route::view('/dashboard', 'admin.dashboard')->name('dashboard');
+
+    Route::view('admin', 'admin.dashboard')->name('admin');
+    Route::get('admin/create', [PostController::class, 'create'])->name('admin.create');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
