ab349eedda8dda656d1c65e198bc6dd383debc52
NafiurSiddiqui <saejim91@gmail.com>
2024-04-28 02:05:28 +0600

    UI (modal): close onClick prohibited on child

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,7 +1,7 @@
 @props(['btn-label'])
-<div x-data="{isOpen: false}">
+<div x-data="{isOpen: true}">
     <section  class="bg-gray-800 text-white flex justify-center items-center absolute w-full h-screen inset-0 bg-opacity-80 backdrop-blur" x-on:click="isOpen = false" x-show="isOpen">
-        <div class="bg-gray-200 w-4/5 text-gray-600 px-4 py-8 rounded-md">
+        <div class="bg-gray-200 w-4/5 text-gray-600 px-4 py-8 rounded-md" x-on:click.stop >
            {{$slot}}
         </div>
     </section>
