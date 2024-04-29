668298792174bd980cc40f8c2bbce31e8e02ca80
NafiurSiddiqui <saejim91@gmail.com>
2024-04-29 22:18:11 +0600

    UI (error): Category check implemented

--- resources/views/components/form/error.blade.php
+++ resources/views/components/form/error.blade.php
@@ -1,5 +1,5 @@
 @props(['name'=>''])
 
 @error($name)
-<p class="text-red-500 mt-2 text-xs">{{ $message }}</p>
+<p class="text-red-500 mt-2 text-xs">{{ $name == 'category_id'? 'Please select a category': $message }}</p>
 @enderror
