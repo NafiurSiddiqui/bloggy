a4ec689fa4eddffbec94a3bd5057c6ccaec62036
NafiurSiddiqui <saejim91@gmail.com>
2024-04-29 22:07:56 +0600

    UI (dashboard): primary and secondary dynamic buttons updated

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,13 +1,13 @@
 @props(['link' => false, 'href'=>''])
 
 @if($link)
-    <a href="{{$href}}"  class="bg-blue-500 block px-10 py-2 font-semibold rounded text-white mt-4 uppercase hover:bg-blue-600 text-xs">
+    <a href="{{$href}}"  class="bg-blue-500 block px-10 py-2 font-semibold rounded text-white mt-4 uppercase hover:bg-blue-600 text-xs" {{$attributes}}>
         {{$slot}}
     </a>
 @else
 
 <button type="submit"
-        class="bg-blue-500 px-10 py-2 font-semibold rounded text-white mt-4 uppercase hover:bg-blue-600 text-xs" >
+        class="bg-blue-500 px-10 py-2 font-semibold rounded text-white mt-4 uppercase hover:bg-blue-600 text-xs" {{$attributes}}>
     {{ $slot }}
 </button>
 @endif
