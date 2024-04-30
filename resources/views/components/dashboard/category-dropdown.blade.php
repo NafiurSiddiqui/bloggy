6538ac0afdd28723cf7c2cef8a4456ee2efb5289
NafiurSiddiqui <saejim91@gmail.com>
2024-05-01 00:44:37 +0600

    UI (category dropdown): clean up and updated

--- resources/views/components/dashboard/category-dropdown.blade.php
+++ resources/views/components/dashboard/category-dropdown.blade.php
@@ -1,4 +1,2 @@
 
-{{--    what happens when there is no cateogories generated yet --}}
-{{--    show a way to create a new category, subcategory --}}
-    <x-dashboard.dropdown :items="$categories" type="category"/>
+ <x-dashboard.dropdown :items="$categories" type="category" />
--- resources/views/components/dashboard/dropdown.blade.php
+++ resources/views/components/dashboard/dropdown.blade.php
@@ -1,16 +1,14 @@
 @props(['items','type'])
 
 @if($items->isEmpty())
-
    <div>
        <p>No item yet!</p>
        <x-secondary-button>Create a {{ucfirst($type)}}</x-secondary-button>
    </div>
 @else
-<div>
+<div class="my-4">
     <x-form.label name="{{$type}}" />
     <select name="{{$type}}_id" id="{{$type}}_id" class="rounded-md w-32" title="Select a {{$type}} for the post">
-        <option value="not selected" >---</option>
         @foreach($items as $item)
             <option value="{{$item->id}}" {{ old(("$type"."_id")) == $item->id ? 'selected':'' }}>{{ucwords($item->name)}}</option>
         @endforeach
