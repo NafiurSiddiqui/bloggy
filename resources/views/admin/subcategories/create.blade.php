22d1dbdfd630ebc4302d28566ab8754a1d872765
NafiurSiddiqui <saejim91@gmail.com>
2024-05-11 23:44:29 +0600

    feature (subcategories): updated btn props

--- resources/views/admin/subcategories/create.blade.php
+++ resources/views/admin/subcategories/create.blade.php
@@ -4,12 +4,12 @@
     </x-slot:heading>
 
     <x-panel>
-        <form  class="subcategory-form" action="/admin/subcategories/store" method="post" >
+        <form class="subcategory-form" action="/admin/subcategories/store" method="post">
             @csrf
-            <x-form.input name="title"/>
-            <x-form.input name="slug"/>
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
             <x-dashboard.category-dropdown />
-            <x-form.action-buttons cancel-href="/admin/subcategories" submit-label="Save" />
+            <x-form.action-buttons secondary-btn-href="/admin/subcategories" submit-label="Save" />
         </form>
     </x-panel>
 </x-dashboard.dashboard-layout>
