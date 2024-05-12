e6999dcd75517b3a1b0534e1ebbb6357c0f766fd
NafiurSiddiqui <saejim91@gmail.com>
2024-05-13 01:52:13 +0600

    UI (danger-button):updated

--- resources/views/components/danger-button.blade.php
+++ resources/views/components/danger-button.blade.php
@@ -1,8 +1,8 @@
-@props(['submit', 'label'])
+@props(['submit', 'label', 'button-only'])
 
-@if (isset($submit))
-    <button type="submit" form="form-delete"
-        {{ $attributes->merge(['class' => 'border border-rose-200 dark:text-rose-500 font-medium hover:text-rose-600  ms-3 px-4 rounded text-rose-400']) }}>{{ $label ?? 'Delete' }}</button>
+@if (isset($submit) || isset($buttonOnly))
+    <button type="{{ isset($submit) ? 'submit' : 'button' }}" form="form-delete"
+        {{ $attributes->merge(['class' => 'border border-rose-200 dark:text-rose-500 font-medium hover:text-rose-600  ms-3 py-2 px-4 rounded text-rose-400']) }}>{{ $label ?? 'Delete' }}</button>
 @else
     <span
         {{ $attributes->merge(['class' => 'mt-4 font-medium  text-rose-400 dark:text-rose-500 hover:underline hover:text-rose-600 cursor-pointer']) }}>{{ $label ?? 'Delete' }}</span>
