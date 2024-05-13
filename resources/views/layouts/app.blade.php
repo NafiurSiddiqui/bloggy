4d26ad0cc4a2b9e7c840ddd0dd77d0997444118a
NafiurSiddiqui <saejim91@gmail.com>
2024-05-13 17:30:53 +0600

    UI (app): overflow-x-clip added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-{{--    NEED FAVICON --}}
-    <title>{{$title?? 'Welcome'}}- {{config('app.name', 'Bloggy')}}</title>
+    {{--    NEED FAVICON --}}
+    <title>{{ $title ?? 'Welcome' }}- {{ config('app.name', 'Bloggy') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,13 +14,15 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-@include('layouts.navigation')
+
+<body class="font-sans antialiased dark:bg-black dark:text-white/50 overflow-x-clip">
+    @include('layouts.navigation')
 
 
-<main >
-    {{$slot}}
-</main>
+    <main>
+        {{ $slot }}
+    </main>
 
 </body>
+
 </html>
