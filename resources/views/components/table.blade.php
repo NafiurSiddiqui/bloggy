fe89ecab64dc9292a85b5b33cf33214995c0d711
NafiurSiddiqui <saejim91@gmail.com>
2024-05-13 17:45:09 +0600

    UI (table): reusable componenet created

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,5 @@
+<table class="w-full divide-y divide-gray-200">
+    <thead class="bg-gray-50">
+        {{ $slot }}
+    </thead>
+</table>
---
+++ resources/views/components/td.blade.php
@@ -0,0 +1,3 @@
+<td {{ $attributes->merge(['class' => ' w-44 px-6 py-4 text-sm']) }}>
+    {{ $slot }}
+</td>
