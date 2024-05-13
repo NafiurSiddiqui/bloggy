85f6e70e35672bf9e238b1532d6d7ef362e721cb
NafiurSiddiqui <saejim91@gmail.com>
2024-05-13 17:45:50 +0600

    cleanup

--- resources/views/components/dashboard/dashboard-layout.blade.php
+++ resources/views/components/dashboard/dashboard-layout.blade.php
@@ -1,19 +1,14 @@
 <x-app-layout>
     <x-slot:title>
-        Dashboard
+        {{ __('Dashboard') }}--}}
     </x-slot:title>
 
-    {{--    <x-slot name="header">--}}
-    {{--        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">--}}
-    {{--            {{ __('Dashboard') }}--}}
-    {{--        </h2>--}}
-    {{--    </x-slot>--}}
 
     <div class="flex min-h-screen ">
         <aside class="hidden lg:flex flex-col w-64 bg-gray-200 dark:bg-gray-900 text-white px-4 py-6">
             <ul class="space-y-2">
                 <x-dashboard.nav-link route="/admin" :active="request()->is('admin')">
-                    Dashboard
+                    {{ __('Dashboard') }}--}}
                 </x-dashboard.nav-link>
 
                 <x-dashboard.nav-link route="/admin/posts" :active="request()->is('admin.posts')">
@@ -34,7 +29,7 @@
             </ul>
         </aside>
         <section class="flex-grow p-4 relative w-full">
-            <h1 class="font-semibold text-xl text-gray-500">{{$heading ?? 'Default heading'}}</h1>
+            <h1 class="font-semibold text-xl text-gray-500">{{ $heading ?? 'Default heading' }}</h1>
             {{ $slot }}
         </section>
     </div>
@@ -42,7 +37,7 @@
 
     @if (session()->has('success'))
         <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-             class="bg-green-500 text-white fixed right-3 bottom-3 p-4 rounded">
+            class="bg-green-500 text-white fixed right-3 bottom-3 p-4 rounded">
             <p>{{ session('success') }}</p>
         </div>
     @endif
