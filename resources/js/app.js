9a96cc6f293530162a5a116994809f2c7f762959
NafiurSiddiqui <saejim91@gmail.com>
2024-05-13 18:00:40 +0600

    cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,33 +7,3 @@ import Alpine from "alpinejs";
 window.Alpine = Alpine;
 
 Alpine.start();
-
-//for handling multiple selection for bulk delete
-const bulkDeleteCheckbox = document.getElementById("bulk_delete");
-const selectColumnHeader = document.querySelector(".select-column");
-const selectAllRows = document.querySelectorAll(".bulk-delete-selection");
-const iconMultipleSelection = document.querySelector(
-    ".multiple-selection-btns"
-);
-const bulkDeleteBtn = document.querySelector(".bulk-delete-btn");
-let bulkDeleteIsChecked = false;
-
-bulkDeleteCheckbox.addEventListener("change", () => {
-    bulkDeleteIsChecked = bulkDeleteCheckbox.checked;
-
-    if (bulkDeleteIsChecked) {
-        selectColumnHeader.classList.remove("hidden");
-        selectAllRows.forEach((element) => {
-            element.classList.remove("hidden");
-        });
-        iconMultipleSelection.classList.add("text-blue-500");
-        bulkDeleteBtn.classList.add("shadow");
-    } else {
-        selectColumnHeader.classList.add("hidden");
-        selectAllRows.forEach((element) => {
-            element.classList.add("hidden");
-        });
-        iconMultipleSelection.classList.remove("text-blue-500");
-        bulkDeleteBtn.classList.remove("shadow");
-    }
-});
