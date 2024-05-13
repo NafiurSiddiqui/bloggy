34e3412dc28c57288e11431549537078fbcda754
NafiurSiddiqui <saejim91@gmail.com>
2024-05-14 02:15:39 +0600

    UI (categories): table updated with blade components

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,33 +1,57 @@
 <x-dashboard.dashboard-layout>
     <x-slot:heading>Categories </x-slot:heading>
-{{--    Show table of categories, edit,delete, no. of posts,--}}
-{{--    maybe you wanna show the subcategories here rather than a separate page.--}}
+    {{--    Show table of categories, edit,delete, no. of posts, --}}
+    {{--    maybe you wanna show the subcategories here rather than a separate page. --}}
 
-<div class="mt-6">
+    <div class="mt-6">
 
-        @if($categories_are_empty)
-        <x-panel>
-            <div class="space-y-4 my-2">
-                <p>No categories created yet</p>
-                <x-secondary-button link href="/admin/categories/create">
-                    Create a Category
-                </x-secondary-button>
-            </div>
-    </x-panel>
+        @if ($categories_are_empty)
+            <x-panel>
+                <div class="space-y-4 my-2">
+                    <p>No categories created yet</p>
+                    <x-secondary-button link href="/admin/categories/create">
+                        Create a Category
+                    </x-secondary-button>
+                </div>
+            </x-panel>
         @else
-            <div class="my-2 flex justify-end">
+            {{-- <div class="my-2 flex justify-end">
 
                 <x-secondary-button link href="/admin/categories/create">
                     Create a Category
                 </x-secondary-button>
-            </div>
+            </div> --}}
+            <x-dashboard.index-actions singular-type="category" plural-type="categories"
+                multiple-delete-form-action-path="delete-multiple-categories"
+                delete-form-action-path="/admin/categories/delete-all" path-to-creation="/admin/categories/create" />
 
             <x-dashboard.categories-table :items="$categories" edit-href="admin/categories" />
 
-        <x-pagination-holder :item="$categories" />
-    @endif
+            <x-pagination-holder :item="$categories" />
+        @endif
 
 
-</div>
+    </div>
 
 </x-dashboard.dashboard-layout>
+
+
+<script>
+    const categoryCheckBoxes = document.querySelectorAll('.category-delete-checkbox');
+    const submitBtn = document.querySelector('.delete-selected-categories-btns');
+
+
+    function checkAnyCheckbox() {
+        // Loop through all postCheckBoxes
+        for (const checkbox of categoryCheckBoxes) {
+            if (checkbox.checked) {
+                submitBtn.classList.remove('hidden');
+                return; // Exit the loop if at least one checkbox is checked
+            }
+        }
+        submitBtn.classList.add('hidden');
+    }
+
+    // Add event listener to each checkbox for change
+    categoryCheckBoxes.forEach(checkbox => checkbox.addEventListener('change', checkAnyCheckbox));
+</script>
--- resources/views/components/dashboard/categories-table.blade.php
+++ resources/views/components/dashboard/categories-table.blade.php
@@ -6,7 +6,7 @@
         <tr>
             <x-th>Select</x-th>
             <x-th>
-                Name
+                title
             </x-th>
             <x-th>
                 Created At
@@ -28,34 +28,38 @@
             @foreach ($items as $item)
                 <tr
                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="w-4 p-4">
-                        <div class="flex items-center">
+                    <x-td>
+                        {{-- <div class="flex items-center">
                             <input id="checkbox-table-1" type="checkbox"
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-table-1" class="sr-only">checkbox</label>
-                        </div>
-                    </td>
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        </div> --}}
+
+                        <x-form.checkbox input-name="bulk_delete_selection[]" id="bulk_delete_selection"
+                            class="{{ $is_subcategories ? 'subcategory' : 'category' }}-delete-checkbox"
+                            value="{{ $item->id }}" checkbox-only />
+                    </x-td>
+                    <x-td>
                         {{ $item->title }}
-                    </th>
-                    <td class="px-6 py-4">
+                    </x-td>
+                    <x-td>
                         {{ $item->created_at->diffForHumans() }}
-                    </td>
-                    <td class="px-6 py-4">
+                    </x-td>
+                    <x-td>
                         {{ $item->updated_at->diffForHumans() }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{--                    {{$item->subcategories->count()}} --}}
+                    </x-td>
+                    <x-td>
+
                         {{ $is_subcategories ? $item->category->title : $item->subcategories->count() }}
-                    </td>
-                    <td class="px-6 py-4">
+                    </x-td>
+                    <x-td>
                         {{ $item->posts->count() }}
-                    </td>
-                    <td class="px-6 py-4 flex space-y-2  lg:space-y-0 flex-col lg:flex-row ">
+                    </x-td>
+                    <x-td>
                         <a href="/{{ $editHref }}/{{ $item->id }}/edit"
                             class=" font-medium w-full text-blue-600 dark:text-blue-500 hover:underline hover:text-blue-600">Edit</a>
 
-                    </td>
+                    </x-td>
                 </tr>
             @endforeach
 
