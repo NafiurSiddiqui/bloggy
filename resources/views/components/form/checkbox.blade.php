753e7d0ea56892add3eb59b661453cc525c87456
NafiurSiddiqui <saejim91@gmail.com>
2024-05-16 04:21:09 +0600

    UI (Checkbox) : conditional render for uncategory implemented

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -5,7 +5,7 @@
 @if (isset($checkboxOnly))
     <div class="flex items-center">
         <input type="checkbox" id="{{ $id }}" name={{ $inputName }}
-            {{ $attributes->merge(['class' => 'w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600']) }}
+            {{ $attributes->merge(['class' => 'w-5 h-5 text-blue-600 bg-gray-50 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 cursor-pointer']) }}
             @checked(old($inputName))>
         <x-form.label label-for="{{ $inputName }}" sr-only />
         <x-form.error name="{{ $inputName }}" class="block mt-1" />
