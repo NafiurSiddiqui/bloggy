493263af03160d9837438f3a2ffb38a55bf26436
NafiurSiddiqui <saejim91@gmail.com>
2024-05-18 13:11:03 +0600

    attributes allowed

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,7 +1,7 @@
  @props(['for-name', 'for-id', 'for-title'])
 
  <select name="{{ $forName }}" id="{{ $forId }}" class="rounded-md w-32   dark:bg-gray-800 dark:text-gray-200"
-     title="{{ $forTitle }}">
+     title="{{ $forTitle }}" {{ $attributes }}>
 
      {{ $slot }}
  </select>
