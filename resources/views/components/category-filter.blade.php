835defbdba13a643f3b6ba200ae74c6c2ba36bb7
NafiurSiddiqui <saejim91@gmail.com>
2024-05-23 02:50:53 +0600

    UI (category-filter): dynamic component return

--- resources/views/components/category-filter.blade.php
+++ resources/views/components/category-filter.blade.php
@@ -1,18 +1,45 @@
-{{-- @props(['formAction', 'isPostsPage' => false]) --}}
+@props(['formAction', 'isPostsPage' => false])
 
-{{-- 
-<form action="{{ $formAction }}" id="filterByCategory" name="filterByCategory" method="get"
-    data-is-posts-page={{ $isPostsPage }}> --}}
-<x-form.label label-for="filter[slug]" label="Filter by Category" />
-<x-select for-name="filter[slug]" for-id="filter[slug]" for-title="filter by categories">
-    <option value="">
-        All
-    </option>
 
-    @foreach ($categories as $category)
-        <option value="{{ $category?->slug }}" {{ $currentCategory?->slug == $category->slug ? 'selected' : '' }}>
-            {{ $category->title }}
+
+@if (isset($formAction))
+    <p>Hello</p>
+    <form x-data="{ selectedOption: '' }" action="{{ $formAction }}" id="filterByCategory" name="filterByCategory" method="get"
+        data-is-posts-page={{ $isPostsPage }} @submit.prevent="submitForm">
+        <x-form.label label-for="filter[slug]" label="Filter by Category" />
+        <x-select for-name="filter[slug]" for-id="filter[slug]" for-title="filter by categories"
+            x-on:change="submitForm()">
+            <option value="">
+                All
+            </option>
+
+            @foreach ($categories as $category)
+                <option value="{{ $category?->slug }}"
+                    {{ $currentCategory?->slug == $category->slug ? 'selected' : '' }}>
+                    {{ $category->title }}
+                </option>
+            @endforeach
+        </x-select>
+    </form>
+@else
+    <p>Hello World!</p>
+    <x-form.label label-for="filter[slug]" label="Filter by Category" />
+    <x-select for-name="filter[slug]" for-id="filter[slug]" for-title="filter by categories">
+        <option value="">
+            All
         </option>
-    @endforeach
-</x-select>
-{{-- </form> --}}
+
+        @foreach ($categories as $category)
+            <option value="{{ $category?->slug }}" {{ $currentCategory?->slug == $category->slug ? 'selected' : '' }}>
+                {{ $category->title }}
+            </option>
+        @endforeach
+    </x-select>
+@endif
+
+
+<script>
+    function submitForm() {
+        document.getElementById('filterByCategory').submit();
+    }
+</script>
