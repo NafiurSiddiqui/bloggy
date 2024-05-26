13038ac273712bd4081d9247af4ec392fc24bbfb
NafiurSiddiqui <saejim91@gmail.com>
2024-05-26 17:50:58 +0600

    UI (sort th): created

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,6 +1,6 @@
 <th scope="col"
-    class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-gray-100 hover:text-gray-500 cursor-pointer border-x border-x-gray-200">
-    <div class="flex justify-between" x-data="{ sort: false }" @click="sort = !sort">
+    class="text-left text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-gray-100 hover:text-gray-500 cursor-pointer border-x border-x-gray-200">
+    <div class="px-2 py-3 flex justify-between" x-data="{ sort: false }" @click="sort = !sort">
         {{ $slot }}
         <div class ="flex flex-col items-center justify-center text-gray-400">
             <i class="fa-solid fa-arrow-down-short-wide" x-show="sort === false"></i>
