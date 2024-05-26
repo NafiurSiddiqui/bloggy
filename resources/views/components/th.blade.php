a8d88ec638a7a6f4f2d7840f4e656ee1415401c1
NafiurSiddiqui <saejim91@gmail.com>
2024-05-26 17:34:35 +0600

    feature (sort columns): wip

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,3 +1,10 @@
-<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-    {{ $slot }}
+<th scope="col"
+    class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-gray-100 hover:text-gray-500 cursor-pointer border-x border-x-gray-200">
+    <div class="flex justify-between" x-data="{ sort: false }" @click="sort = !sort">
+        {{ $slot }}
+        <div class ="flex flex-col items-center justify-center text-gray-400">
+            <i class="fa-solid fa-arrow-down-short-wide" x-show="sort === false"></i>
+            <i class="fa-solid fa-arrow-up-short-wide" x-show="sort === true"></i>
+        </div>
+    </div>
 </th>
