316282ea28b88adafff764738ed5416800f08359
NafiurSiddiqui <saejim91@gmail.com>
2024-05-26 23:02:05 +0600

    updated sort icon

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,10 +1,31 @@
+@props(['sort-by', 'th-title'])
+
+@php
+    $sortAsc = isset($sortBy) && request()->has('sort') && request('dir') == 'asc';
+
+    $sortDesc = isset($sortBy) && request()->has('sort') && request('dir') == 'desc';
+
+    $isPostsHomePage = empty(request()->query());
+
+@endphp
+
+
 <th scope="col"
     class="text-left text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-gray-100 hover:text-gray-500 cursor-pointer border-x border-x-gray-200">
-    <div class="px-2 py-3 flex justify-between" x-data="{ sort: false }" @click="sort = !sort">
-        {{ $slot }}
+    <div class="{{ isset($sortBy) ? '' : 'py-3' }} px-2 flex justify-between">
+        @if ($sortAsc)
+            <a href="/admin/posts/?sort=-{{ $sortBy }}&dir=desc" class=" py-3 w-full inline-block h-9 text-center">
+                {{ $thTitle }}
+            </a>
+        @elseif(isset($sortBy))
+            <a href="/admin/posts/?sort={{ $sortBy }}&dir=asc" class=" py-3 w-full inline-block  h-9 text-center">
+                {{ $thTitle }}
+            </a>
+        @else
+            {{ $thTitle }}
+        @endif
         <div class ="flex flex-col items-center justify-center text-gray-400">
-            <i class="fa-solid fa-arrow-down-short-wide" x-show="sort === false"></i>
-            <i class="fa-solid fa-arrow-up-short-wide" x-show="sort === true"></i>
+            <i class="fa-solid fa-sort"></i>
         </div>
     </div>
 </th>
