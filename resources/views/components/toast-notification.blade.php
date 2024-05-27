12ea40fa821210792062b9a6513c690c2ab60a90
NafiurSiddiqui <saejim91@gmail.com>
2024-05-28 04:38:40 +0600

    UI : Toast notification created

---
+++ resources/views/components/toast-notification.blade.php
@@ -0,0 +1,15 @@
+@props(['type-message', 'message'])
+
+@if (isset($typeMessage))
+    <div x-data="{ show: true }" x-show="show"
+        class="border-2 border-blue-500 shadow-lg bg-blue-500 font-semibold fixed right-3 bottom-5 p-4 rounded transition duration-300 ease-in-out flex justify-between items-center gap-4"
+        @click.outside="show = false">
+
+        <p class=" text-gray-100 ">{{ isset($message) ? $message : 'Supposed to be informative' }}</p>
+        <button @click="show = false">
+            <i
+                class="fas fa-times text-blue-200 text-xl hover:text-gray-
+        600 transition duration-300 ease-in-out px-2 py-1 rounded-md hover:text-gray-500 hover:bg-gray-200 cursor-pointer"></i>
+        </button>
+    </div>
+@endif
