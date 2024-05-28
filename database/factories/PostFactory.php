65070c1bc2c4dd27415b376d2a1878baedf95bab
NafiurSiddiqui <saejim91@gmail.com>
2024-05-28 21:13:26 +0600

    duplicate value for status prevented

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,10 +16,40 @@ class PostFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    // public function definition(): array
+    // {
+    //     $category = Category::factory()->create();
+    //     $subcategory = Subcategory::factory()->create(['category_id' => $category->id]);
+    //     return [
+    //         'user_id' => \App\Models\User::factory([
+    //             'role' => 'editor'
+    //         ]),
+    //         'category_id' => $category->id,
+    //         'subcategory_id' => $subcategory->id,
+    //         'title' => $this->faker->unique()->sentence,
+    //         'slug' => $this->faker->unique()->slug,
+    //         'description' => $this->faker->text,
+    //         'body' => $this->faker->paragraph,
+    //         'thumbnail' => $this->faker->imageUrl(),
+    //         'thumbnail_alt_txt' => $this->faker->text,
+    //         'is_published' => $this->faker->boolean,
+    //         'is_draft' => $this->faker->boolean,
+    //         'is_unpublished' => $this->faker->boolean,
+    //         'is_featured' => $this->faker->randomElement(['on', 'off']),
+    //         'is_hot' => $this->faker->randomElement(['on', 'off']),
+    //         'meta_title' => $this->faker->sentence,
+    //         'meta_description' => $this->faker->text,
+    //         'og_thumbnail' => $this->faker->optional()->imageUrl(),
+    //         'og_title' => $this->faker->optional()->sentence,
+    //     ];
+    // }
     public function definition(): array
     {
         $category = Category::factory()->create();
         $subcategory = Subcategory::factory()->create(['category_id' => $category->id]);
+
+        $publicationStatus = $this->faker->randomElement(['published', 'draft', 'unpublished']);
+
         return [
             'user_id' => \App\Models\User::factory([
                 'role' => 'editor'
@@ -32,9 +62,9 @@ class PostFactory extends Factory
             'body' => $this->faker->paragraph,
             'thumbnail' => $this->faker->imageUrl(),
             'thumbnail_alt_txt' => $this->faker->text,
-            'is_published' => $this->faker->boolean,
-            'is_draft' => $this->faker->boolean,
-            'is_unpublished' => $this->faker->boolean,
+            'is_published' => $publicationStatus === 'published',
+            'is_draft' => $publicationStatus === 'draft',
+            'is_unpublished' => $publicationStatus === 'unpublished',
             'is_featured' => $this->faker->randomElement(['on', 'off']),
             'is_hot' => $this->faker->randomElement(['on', 'off']),
             'meta_title' => $this->faker->sentence,
