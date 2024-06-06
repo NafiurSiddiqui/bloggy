8cdf0f6028e67c0000680fc859032d83212665ec
NafiurSiddiqui <saejim91@gmail.com>
2024-06-06 21:36:58 +0600

    Model::ShouldBeStrict added for development

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Category;
 use App\Models\Subcategory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
@@ -25,19 +26,20 @@ class AppServiceProvider extends ServiceProvider
     {
 
 
-        if(Schema::hasTable('categories')){
-            View::share('categories_are_empty',!Category::exists());
-        }else{
+        if (Schema::hasTable('categories')) {
+            View::share('categories_are_empty', !Category::exists());
+        } else {
 
             View::share('categories_are_empty', 'Category database is not migrated yet!');
         }
 
-        if(Schema::hasTable('subcategories')){
-            View::share('subcategories_are_empty',!Subcategory::exists());
-        }else{
+        if (Schema::hasTable('subcategories')) {
+            View::share('subcategories_are_empty', !Subcategory::exists());
+        } else {
             View::share('subcategories_are_empty', 'Category database is not migrated yet!');
         }
 
-
+        // TODO: Remove below in production.
+        Model::shouldBeStrict();
     }
 }
