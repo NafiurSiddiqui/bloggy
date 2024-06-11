f8bb13ff2fb9274ea6cc1b9cd1a8185ba3d10ffa
NafiurSiddiqui <saejim91@gmail.com>
2024-06-11 21:42:28 +0600

    clean up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,35 @@
 <x-app-layout>
     <main class="borderTest">
-            @if(isset($featured) && $featured)
+        @if (isset($featured) && $featured)
             <section>
                 Featured Posts
-                @foreach($featured as $post)
+                @foreach ($featured as $post)
                     <x-post-card :post="$post" />
                 @endforeach
 
             </section>
-            @endif
-        @if(isset($hot) && $hot)
-        <section>
-            Hot
-            @foreach($hot as $post)
-                <x-post-card :post="$post" />
-            @endforeach
-        </section>
         @endif
-        @if(isset($posts) && $posts)
-        <section>
-            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-                <h1 class="text-2xl font-bold">All Posts</h1>
-                <p>There are {{$post_count}} posts here</p>
-                @foreach ($posts as $post)
-                    <x-post-card :post="$post"/>
-
-            </div>
+        @if (isset($hot) && $hot)
+            <section>
+                Hot
+                @foreach ($hot as $post)
+                    <x-post-card :post="$post" />
                 @endforeach
+            </section>
+        @endif
+        @if (isset($posts) && $posts)
+            <section>
+                <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    <h1 class="text-2xl font-bold">All Posts</h1>
+                    <p>There are {{ $post_count }} posts here</p>
+                    @foreach ($posts as $post)
+                        <x-post-card :post="$post" />
+
+                </div>
+        @endforeach
         </section>
-                @else
-            <p>No posts yet. stay tuned for upcoming posts.</p>
+    @else
+        <p>No posts yet. stay tuned for upcoming posts.</p>
         @endif
 
 
@@ -42,6 +42,5 @@
     <footer class="borderTest">
         The footer
     </footer>
-{{--    </body>--}}
-{{--    </html>--}}
+
 </x-app-layout>
