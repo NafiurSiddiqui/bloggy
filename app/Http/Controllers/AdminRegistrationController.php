e17df63db5caf5e4daedb7350aeea47934efc9a7
NafiurSiddiqui <saejim91@gmail.com>
2024-06-15 23:15:45 +0600

    cleanup

--- app/Http/Controllers/AdminRegistrationController.php
+++ app/Http/Controllers/AdminRegistrationController.php
@@ -3,11 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Hash;
 
