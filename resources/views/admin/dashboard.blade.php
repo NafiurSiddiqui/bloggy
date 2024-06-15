88a16eb8b1ba78f4563dbe2dde61fbd8bfc5000a
NafiurSiddiqui <saejim91@gmail.com>
2024-06-16 00:50:42 +0600

    UI: Routes proected

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,8 @@
 <x-dashboard.dashboard-layout>
     <x-slot:heading>Dashboard</x-slot:heading>
+    {{-- Here you need to notify if there is any application pending --}}
+    <div>
+        Notifications:
+    </div>
+
 </x-dashboard.dashboard-layout>
