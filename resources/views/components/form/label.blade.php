a9a3b7f8656b72faa0118181392f2f0bf32ba148
NafiurSiddiqui <saejim91@gmail.com>
2024-06-20 21:31:02 +0600

    UI : updated

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,9 +1,8 @@
 @props(['label-for', 'label', 'sr-only'])
 
 
-@dd($srOnly)
 @if (isset($srOnly))
-    <label for="{{ $labelFor }}" class="sr-only">{{ $lableFor }}</label>
+    <label for="{{ $labelFor }}" class="sr-only">{{ $labelFor }}</label>
 @else
     <label for="{{ $labelFor }}"
         {{ $attributes->merge(['class' => 'block mb-2 text-sm font-medium text-gray-600  dark:text-gray-400']) }}>{{ ucwords($label ?? $labelFor) }}</label>
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,11 +1,29 @@
 @props(['name' => '', 'required' => false, 'sr-only' => ''])
 
 
+@php
+    $requiredClasses = 'block mb-2 text-sm font-medium text-gray-600  dark:text-gray-400';
+    if ($required) {
+        $requiredClasses =
+            'block mb-2 text-sm font-medium text-gray-600  dark:text-gray-400 after:content-[\'*\'] after:text-red-500';
+    }
+
+    if (isset($srOnly) || ($required && isset($srOnly))) {
+        $requiredClasses = 'sr-only';
+    }
+
+    if (!isset($srOnly) && !$required) {
+        $requiredClasses = 'block mb-2 text-sm font-medium text-gray-600  dark:text-gray-400';
+    }
+
+    // dd($name);
+
+@endphp
+
 <x-form.field>
-    <x-form.label label-for="{{ $name }}"
-        class="{{ $required ? 'after:content-[\'*\'] after:text-red-500' : '' }}"{{ isset($srOnly) ? $srOnly : '' }} />
+    <x-form.label label-for="{{ $name }}" class="{{ $requiredClasses }}" sr-only="{{ isset($srOnly) }}" />
     <textarea name="{{ $name }}" rows="5"
-        class="w-full p-2 text-xs border border-gray-300 focus:outline-none focus:ring rounded  dark:bg-gray-800 dark:text-gray-200 dark:border-gray-700"
+        class="w-full  text-xs border border-gray-300 focus:outline-none focus:ring rounded  dark:bg-gray-800 dark:text-gray-200 dark:border-gray-700"
         {{ $attributes }} {{ $required ? 'required' : '' }}>
 
     {{ $slot ?? old($name) }}
