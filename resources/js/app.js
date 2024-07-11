4d5f0049efd2bd7f7bbee88258bc4f5de2dc3fe7
NafiurSiddiqui <saejim91@gmail.com>
2024-07-12 02:32:31 +0600

    fontawesome brands import removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import "./bootstrap";
 import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/free-brands-svg-icons";
 import "@fortawesome/fontawesome-free/js/solid";
 
 import Alpine from "alpinejs";
