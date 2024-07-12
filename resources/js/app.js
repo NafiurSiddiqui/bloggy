6e72539520c796b5bd9126368b59abe3f06f6468
NafiurSiddiqui <saejim91@gmail.com>
2024-07-13 00:25:05 +0600

    feature (editor): udpated ckeditor [202]

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,412 @@
 import "./bootstrap";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
-
 import Alpine from "alpinejs";
+import {
+    ClassicEditor,
+    AccessibilityHelp,
+    Autoformat,
+    AutoImage,
+    AutoLink,
+    Autosave,
+    BalloonToolbar,
+    BlockQuote,
+    BlockToolbar,
+    Bold,
+    Code,
+    CodeBlock,
+    Essentials,
+    FindAndReplace,
+    // FileRepository,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    FullPage,
+    Heading,
+    Highlight,
+    HorizontalLine,
+    ImageBlock,
+    ImageCaption,
+    ImageInline,
+    ImageInsertViaUrl,
+    ImageResize,
+    ImageStyle,
+    ImageTextAlternative,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    ListProperties,
+    Markdown,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    SelectAll,
+    ShowBlocks,
+    SourceEditing,
+    SpecialCharacters,
+    SpecialCharactersArrows,
+    SpecialCharactersCurrency,
+    SpecialCharactersEssentials,
+    SpecialCharactersLatin,
+    SpecialCharactersMathematical,
+    SpecialCharactersText,
+    Strikethrough,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    TodoList,
+    Underline,
+    Undo,
+} from "ckeditor5";
+import "ckeditor5/ckeditor5.css";
+
+class MyUploadAdapter {
+    constructor(loader) {
+        // The file loader instance to use during the upload.
+        this.loader = loader;
+    }
+
+    // Starts the upload process.
+    upload() {
+        return this.loader.file.then(
+            (file) =>
+                new Promise((resolve, reject) => {
+                    this._initRequest();
+                    this._initListeners(resolve, reject, file);
+                    this._sendRequest(file);
+                })
+        );
+    }
+
+    // Aborts the upload process.
+    abort() {
+        if (this.xhr) {
+            this.xhr.abort();
+        }
+    }
+
+    // Initializes the XMLHttpRequest object using the URL passed to the constructor.
+    _initRequest() {
+        const xhr = (this.xhr = new XMLHttpRequest());
+        const csrfToken = document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute("content");
+        // console.log(csrfToken);
+        xhr.open("POST", "http://localhost/admin/images/", true);
+        xhr.setRequestHeader("x-csrf-token", `${csrfToken}`);
+        xhr.responseType = "json";
+    }
+
+    // Initializes XMLHttpRequest listeners.
+    _initListeners(resolve, reject, file) {
+        const xhr = this.xhr;
+        const loader = this.loader;
+        const genericErrorText = `Couldn't upload file: ${file.name}.`;
+
+        xhr.addEventListener("error", () => reject(genericErrorText));
+        xhr.addEventListener("abort", () => reject());
+        xhr.addEventListener("load", () => {
+            const response = xhr.response;
+
+            if (!response || response.error) {
+                return reject(
+                    response && response.error
+                        ? response.error.message
+                        : genericErrorText
+                );
+            }
+
+            resolve({
+                default: response.url,
+            });
+        });
+
+        if (xhr.upload) {
+            xhr.upload.addEventListener("progress", (evt) => {
+                if (evt.lengthComputable) {
+                    loader.uploadTotal = evt.total;
+                    loader.uploaded = evt.loaded;
+                }
+            });
+        }
+    }
+
+    // Prepares the data and sends the request.
+    _sendRequest(file) {
+        // Prepare the form data.
+        const data = new FormData();
+
+        data.append("upload", file);
+
+        // Send the request.
+        this.xhr.send(data);
+    }
+}
+function MyCustomUploadAdapterPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+        // Configure the URL to the upload script in your back-end here!
+        return new MyUploadAdapter(loader);
+    };
+}
+const editorConfig = {
+    toolbar: {
+        items: [
+            "undo",
+            "redo",
+            "|",
+            "sourceEditing",
+            "showBlocks",
+            "|",
+            "heading",
+            "|",
+            "fontSize",
+            "fontFamily",
+            "fontColor",
+            "fontBackgroundColor",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "|",
+            "link",
+            "insertImageViaUrl",
+            "insertTable",
+            "highlight",
+            "blockQuote",
+            "codeBlock",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "todoList",
+            "indent",
+            "outdent",
+        ],
+        shouldNotGroupWhenFull: false,
+    },
+    plugins: [
+        AccessibilityHelp,
+        Autoformat,
+        AutoImage,
+        AutoLink,
+        Autosave,
+        BalloonToolbar,
+        BlockQuote,
+        BlockToolbar,
+        Bold,
+        Code,
+        CodeBlock,
+        Essentials,
+        FindAndReplace,
+        FontBackgroundColor,
+        FontColor,
+        FontFamily,
+        FontSize,
+        FullPage,
+        Heading,
+        Highlight,
+        HorizontalLine,
+        ImageBlock,
+        ImageCaption,
+        ImageInline,
+        ImageInsertViaUrl,
+        ImageResize,
+        ImageStyle,
+        ImageTextAlternative,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        IndentBlock,
+        Italic,
+        Link,
+        LinkImage,
+        List,
+        ListProperties,
+        Markdown,
+        MyCustomUploadAdapterPlugin,
+        MediaEmbed,
+        Paragraph,
+        PasteFromOffice,
+        RemoveFormat,
+        SelectAll,
+        ShowBlocks,
+        SourceEditing,
+        SpecialCharacters,
+        SpecialCharactersArrows,
+        SpecialCharactersCurrency,
+        SpecialCharactersEssentials,
+        SpecialCharactersLatin,
+        SpecialCharactersMathematical,
+        SpecialCharactersText,
+        Strikethrough,
+        Table,
+        TableCaption,
+        TableCellProperties,
+        TableColumnResize,
+        TableProperties,
+        TableToolbar,
+        TextTransformation,
+        TodoList,
+        Underline,
+        Undo,
+    ],
+    balloonToolbar: [
+        "bold",
+        "italic",
+        "|",
+        "link",
+        "|",
+        "bulletedList",
+        "numberedList",
+    ],
+    blockToolbar: [
+        "fontSize",
+        "fontColor",
+        "fontBackgroundColor",
+        "|",
+        "bold",
+        "italic",
+        "|",
+        "link",
+        "insertTable",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "indent",
+        "outdent",
+    ],
+    fontFamily: {
+        supportAllValues: true,
+    },
+    fontSize: {
+        options: [10, 12, 14, "default", 18, 20, 22],
+        supportAllValues: true,
+    },
+    heading: {
+        options: [
+            {
+                model: "paragraph",
+                title: "Paragraph",
+                class: "ck-heading_paragraph",
+            },
+            {
+                model: "heading1",
+                view: "h1",
+                title: "Heading 1",
+                class: "ck-heading_heading1",
+            },
+            {
+                model: "heading2",
+                view: "h2",
+                title: "Heading 2",
+                class: "ck-heading_heading2",
+            },
+            {
+                model: "heading3",
+                view: "h3",
+                title: "Heading 3",
+                class: "ck-heading_heading3",
+            },
+            {
+                model: "heading4",
+                view: "h4",
+                title: "Heading 4",
+                class: "ck-heading_heading4",
+            },
+            {
+                model: "heading5",
+                view: "h5",
+                title: "Heading 5",
+                class: "ck-heading_heading5",
+            },
+            {
+                model: "heading6",
+                view: "h6",
+                title: "Heading 6",
+                class: "ck-heading_heading6",
+            },
+        ],
+    },
+    image: {
+        toolbar: [
+            "toggleImageCaption",
+            "imageTextAlternative",
+            "|",
+            "imageStyle:inline",
+            "imageStyle:wrapText",
+            "imageStyle:breakText",
+            "|",
+            "resizeImage",
+        ],
+    },
+
+    link: {
+        addTargetToExternalLinks: true,
+        defaultProtocol: "https://",
+        decorators: {
+            toggleDownloadable: {
+                mode: "manual",
+                label: "Downloadable",
+                attributes: {
+                    download: "file",
+                },
+            },
+        },
+    },
+    list: {
+        properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true,
+        },
+    },
+    menuBar: {
+        isVisible: true,
+    },
+    placeholder: "Type or paste your content here!",
+    table: {
+        contentToolbar: [
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "tableProperties",
+            "tableCellProperties",
+        ],
+    },
+};
+
+// get the current url
+const url = window.location.href;
+// should not run the following where editor is not used
+// let editor = null;
+
+if (
+    url.includes("post/create") ||
+    (url.includes("/post") && url.includes("/edit"))
+) {
+    document.addEventListener("DOMContentLoaded", function () {
+        ClassicEditor.create(document.querySelector("#editor"), editorConfig)
+            // .then((newEditor) => {
+            //     editor = newEditor;
+            // })
+            .catch((error) => {
+                console.log(error);
+            });
+    });
+}
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// export { AccessibilityHelp };
--- resources/views/components/editor/ck-editor.blade.php
+++ resources/views/components/editor/ck-editor.blade.php
@@ -1,3 +1,15 @@
 @props(['post'])
 
-<textarea id="editor" name="body">{{ isset($post) ? old('body', $post->body) : old('body') }}</textarea>
+{{-- <textarea  id="editor" name="body">{{ isset($post) ? old('body', $post->body) : old('body') }}</textarea> --}}
+
+
+<div>
+    <div class="main-container">
+        <div class="editor-container editor-container_classic-editor editor-container_include-block-toolbar"
+            id="editor-container">
+            <div class="editor-container__editor">
+                <textarea id="editor" name="body">{{ isset($post) ? old('body', $post->body) : old('body') }}</textarea>
+            </div>
+        </div>
+    </div>
+</div>
