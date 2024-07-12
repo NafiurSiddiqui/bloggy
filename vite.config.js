73e75957b8b323fa4b9e0b27bda0c3a68ec20f95
NafiurSiddiqui <saejim91@gmail.com>
2024-07-12 23:26:41 +0600

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                // " resources/views/components/editor/ck-editor.blade.php",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
