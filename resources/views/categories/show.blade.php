6024308c66d4fcdec4c2d308f9e4e35ae5770cb3
NafiurSiddiqui <saejim91@gmail.com>
2024-07-15 00:19:26 +0600

    Feature (post by category): wip

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <h1 class="font-semibold">Category:
+    {{-- <h1 class="font-semibold">Category:
         <span>{{ $category->title }}</span>
     </h1>
 
@@ -11,5 +11,27 @@
             <h1>{{ $post->slug }}</h1>
             <p>{{ $post->author->name }}</p>
         </div>
-    @endforeach
+    @endforeach --}}
+
+    @if ($category->posts)
+        <main class=" p-4 md:px-6 2xl:w-4/5">
+
+            <section>
+                <x-h2>Category: {{ $category->title }}</x-h2>
+                {{-- <p>There are {{ $post_count }} posts here</p> --}}
+                <div class="space-y-4 gap-2 md:px-8 xl:px-0  xl:grid grid-cols-2">
+                    @foreach ($category->posts as $post)
+                        <x-post-cards.card-x :post="$post" />
+                    @endforeach
+                </div>
+                {{-- @if ($posts->count() > 8)
+                    <x-post-cards.section-cta link="posts.all">
+                        💫 See All Posts 💫
+                    </x-post-cards.section-cta>
+                @endif --}}
+            </section>
+        @else
+            <p>No posts yet. stay tuned for upcoming posts.</p>
+    @endif
+    </main>
 </x-app-layout>
--- resources/views/subcategories/show.blade.php
+++ resources/views/subcategories/show.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     {{--    @dd($subcategory) --}}
+    This is Subcategory
     <h1>{{ $category->title }}/{{ $subcategory->title }}</h1>
 
     {{ $subcategory }}
+
+
 </x-app-layout>
