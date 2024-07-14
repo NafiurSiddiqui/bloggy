b1e36244221c6bafe607ca2f9c5bef63d18d6757
NafiurSiddiqui <saejim91@gmail.com>
2024-07-15 03:18:37 +0600

    http_build_query to prevent adding 'categories' in the URL

--- resources/views/components/labels/subcategory.blade.php
+++ resources/views/components/labels/subcategory.blade.php
@@ -2,7 +2,9 @@
 
 
 @if (isset($subcategory) && isset($category) && ($category !== null && $subcategory !== null))
-    <x-labels.layout url="categories/{{ $category->slug }}/{{ $subcategory->slug }}" :sm="$sm">
+    <x-labels.layout
+        url="{{ http_build_query(request()->except('categories', 'page')) }}/categories/{{ $category->slug }}/{{ $subcategory->slug }}"
+        :sm="$sm">
         {{ $subcategory->title }}
     </x-labels.layout>
 @endif
