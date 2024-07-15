9c76b0f4ef764d3fea769e331dc72cad21fc765d
NafiurSiddiqui <saejim91@gmail.com>
2024-07-16 00:46:25 +0600

    nullable slug added

--- database/migrations/2024_04_15_171434_create_categories_table.php
+++ database/migrations/2024_04_15_171434_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('title')->unique();
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->nullable();
         });
     }
 
