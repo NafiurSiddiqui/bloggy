4d554ff225e31af54c5c17e0af2dffdf9605102c
NafiurSiddiqui <saejim91@gmail.com>
2024-07-23 13:02:11 +0600

    Feature (SEO): meta tags added on posts by category, subcat and author

--- resources/views/posts/author/index.blade.php
+++ resources/views/posts/author/index.blade.php
@@ -1,3 +1,16 @@
 <x-app-layout>
+    <x-slot:head>
+        <title>All posts by {{ $author->name }}- {{ env('APP_NAME') }} - Posts </title>
+        <meta name="description" content="Latest news on {{ $author->name }}.">
+        {{-- essential social media og tags --}}
+        <meta property="og:title" content="{{ $author->name }}- {{ env('APP_NAME') }} - Posts">
+        <meta property="og:description" content="Latest news on {{ $author->name }}">
+        {{-- <meta property="og:image" content="" /> --}}
+        {{-- <meta property="og:image:alt" content="" /> --}}
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:url" content="{{ url("/author/$author->name/posts") }}" />
+    </x-slot:head>
+
     <x-show-posts :posts="$posts" header="author" :title="$author->name" no-href-author />
 </x-app-layout>
--- resources/views/posts/subcategories/index.blade.php
+++ resources/views/posts/subcategories/index.blade.php
@@ -1,3 +1,17 @@
 <x-app-layout>
+
+    <x-slot:head>
+        <title>{{ $subcategory->title }}- {{ env('APP_NAME') }} - Posts </title>
+        <meta name="description" content="Latest news on {{ $subcategory->title }}.">
+        {{-- essential social media og tags --}}
+        <meta property="og:title" content="{{ $subcategory->title }}- {{ env('APP_NAME') }} - Posts">
+        <meta property="og:description" content="Latest news on {{ $subcategory->title }}">
+        {{-- <meta property="og:image" content="" /> --}}
+        {{-- <meta property="og:image:alt" content="" /> --}}
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:url"
+            content="{{ url('/categories/' . $subcategory->category->slug . '/' . $subcategory->slug) }}" />
+    </x-slot:head>
     <x-show-posts :posts="$posts" :subcategory="$subcategory" />
 </x-app-layout>
