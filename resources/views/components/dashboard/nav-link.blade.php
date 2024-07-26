aaeb4b24769a28d2f6c396f8639b4d63f81923bd
NafiurSiddiqui <saejim91@gmail.com>
2024-07-26 18:43:44 +0530

    UI : updated style

--- resources/views/components/dashboard/nav-link.blade.php
+++ resources/views/components/dashboard/nav-link.blade.php
@@ -3,5 +3,5 @@
 
 <li class="">
     <a href="{{ isset($route) ? $route : '#' }}"
-        class="text-stone-500  hover:text-gray-600 hover:bg-gray-300 font-bold focus:outline-none focus:text-gray-300 p-2 w-full inline-block rounded-sm {{ isset($active) && $active ? 'bg-stone-200 text-stone-600' : '' }}">{{ $slot }}</a>
+        class="text-stone-500  hover:text-gray-600 hover:bg-stone-200 font-bold focus:outline-none focus:text-stone-700 p-2 w-full inline-block rounded-sm {{ isset($active) && $active ? 'bg-stone-200 text-stone-600' : '' }}">{{ $slot }}</a>
 </li>
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -2,12 +2,12 @@
 
 @if ($link)
     <a href="{{ $href }}"
-        {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center justify-center px-4 py-4 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-sm text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 text-center ']) }}>
+        {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center justify-center px-4 py-4 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-sm text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 text-center']) }}>
         {{ $slot }}
     </a>
 @else
     <button
-        {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex  justify-center px-4 py-4 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md  font-semibold text-sm text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 text-center ']) }}>
+        {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex  justify-center px-4 py-4 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md  font-semibold text-sm text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 text-center ']) }}>
         {{ $slot }}
     </button>
 @endif
