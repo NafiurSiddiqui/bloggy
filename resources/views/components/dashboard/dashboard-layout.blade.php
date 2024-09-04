8b2d12a99322ec30bbf1aa42eee419362d34e1ad
NafiurSiddiqui <saejim91@gmail.com>
2024-09-04 20:42:47 +0600

    UI (dashboard): sidebar dark-mode applied

--- resources/views/components/dashboard/dashboard-layout.blade.php
+++ resources/views/components/dashboard/dashboard-layout.blade.php
@@ -6,29 +6,32 @@
 
 
     <div class="flex min-h-screen w-full ">
-        <aside class="hidden lg:flex flex-col w-64 bg-stone-100 dark:bg-gray-900 text-white px-4 py-6">
-            <ul class="space-y-2">
-                <x-dashboard.nav-link route="/admin" :active="request()->is('admin')">
+        <aside
+            class="hidden lg:flex flex-col w-64 bg-stone-100 
+            dark:bg-darkNavFooter 
+            dark:border-r dark:border-zinc-800 text-white px-4 py-6">
+            <ul class="space-y-2  dark:bg-darkPostCard ">
+                <x-responsive-nav-link href="/admin" :active="request()->is('admin')">
                     {{ __('Dashboard') }}
-                </x-dashboard.nav-link>
+                </x-responsive-nav-link>
 
-                <x-dashboard.nav-link route="/admin/post/create" :active="request()->is('admin/post/create')">
+                <x-responsive-nav-link href="/admin/post/create" :active="request()->is('admin/post/create')">
                     Create post
-                </x-dashboard.nav-link>
+                </x-responsive-nav-link>
 
-                <x-dashboard.nav-link route="/admin/posts" :active="request()->is('admin.posts')">
+                <x-responsive-nav-link href="/admin/posts" :active="request()->is('admin.posts')">
                     All Posts
-                </x-dashboard.nav-link>
+                </x-responsive-nav-link>
 
 
 
-                <x-dashboard.nav-link route="/admin/categories" :active="request()->is('admin/categories')">
+                <x-responsive-nav-link href="/admin/categories" :active="request()->is('admin/categories')">
                     Categories
-                </x-dashboard.nav-link>
+                </x-responsive-nav-link>
 
-                <x-dashboard.nav-link route="/admin/subcategories" :active="request()->is('admin/subcategories')">
+                <x-responsive-nav-link href="/admin/subcategories" :active="request()->is('admin/subcategories')">
                     Sub-categories
-                </x-dashboard.nav-link>
+                </x-responsive-nav-link>
             </ul>
         </aside>
         <section class="flex-grow p-4 relative w-full">
--- resources/views/components/dashboard/nav-link.blade.php
+++ resources/views/components/dashboard/nav-link.blade.php
@@ -1,7 +1,18 @@
 @props(['route', 'active'])
 
+@php
+    $classes =
+        $active ?? false
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-emerald-700 dark:border-emerald-600 text-start text-base font-medium text-indigo-700 dark:text-gray-200 bg-indigo-50 dark:bg-darkTextHover-600/10 focus:outline-none focus:text-emerald-800 dark:focus:text-emerald-200 focus:bg-emerald-100 dark:focus:bg-emerald-900 focus:border-emerald-700 dark:focus:border-emerald-300 transition duration-150 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-zinc-600 dark:text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200 hover:bg-stone-50 dark:hover:bg-darkTextHover-600/10 hover:border-emerald-300 dark:hover:border-emerald-600 focus:outline-none focus:text-zinc-800 dark:focus:text-zinc-200 focus:bg-zinc-50 dark:focus:bg-emerald-700 focus:border-emerald-300 dark:focus:border-emerald-600 transition duration-150 ease-in-out';
+@endphp
 
 <li class="">
-    <a href="{{ isset($route) ? $route : '#' }}"
-        class="text-stone-500  hover:text-gray-600 hover:bg-stone-200 font-bold focus:outline-none focus:text-stone-700 p-2 w-full inline-block rounded-sm {{ isset($active) && $active ? 'bg-stone-200 text-stone-600' : '' }}">{{ $slot }}</a>
+    {{-- <a href="{{ isset($route) ? $route : '#' }}"
+        class="block w-full ps-3 pe-4 py-2 border-l-4 border-emerald-700 dark:border-emerald-600 text-start text-base font-medium text-indigo-700 dark:text-gray-200 bg-indigo-50 dark:bg-darkTextHover-600/10 focus:outline-none focus:text-emerald-800 dark:focus:text-emerald-200 focus:bg-emerald-100 dark:focus:bg-emerald-900 focus:border-emerald-700 dark:focus:border-emerald-300 transition duration-150 ease-in-out">{{ $slot }}</a> --}}
+    {{-- <a href="{{ isset($route) ? $route : '#' }}"
+        class="text-stone-500  hover:text-gray-600 hover:bg-stone-200 dark:hover:text-gray-200 hover:dark:bg-darkNavFooter font-bold focus:outline-none focus:text-stone-700 p-2 w-full inline-block rounded-sm {{ isset($active) && $active ? 'bg-stone-200 text-stone-600 dark:bg-' : '' }}">{{ $slot }}</a> --}}
+    <a {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </a>
 </li>
