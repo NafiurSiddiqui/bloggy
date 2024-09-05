9ffd5504bd9a996891b76a64d1f64d1ac8b13c9a
NafiurSiddiqui <saejim91@gmail.com>
2024-09-05 17:48:51 +0600

    cleanup

--- resources/views/components/editor/ck-editor.blade.php
+++ resources/views/components/editor/ck-editor.blade.php
@@ -13,25 +13,3 @@
         </div>
     </div>
 </div>
-
-
-<script>
-    // ck.ck-menu-bar
-    //ck.ck-toolbar.ck-toolbar_grouping
-    // ck-blurred ck ck-content ck-editor__editable ck-rounded-corners ck-editor__editable_inline
-
-    //upon dom loaded, add 'py-1' to the div that has the class ck.ck-menu-bar
-    document.addEventListener('DOMContentLoaded', function() {
-        const menuBar = document.querySelector('.ck-menu-bar');
-        const toolbar = document.querySelector('.ck.ck-toolbar.ck-toolbar_grouping');
-        const editor = document.querySelector('.ck-blurred.ck .ck-content .ck-editor__editable');
-
-        console.log([
-            menuBar,
-            toolbar,
-            editor
-        ]);
-
-
-    });
-</script>
