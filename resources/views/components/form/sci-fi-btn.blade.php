b6e782d7dcc1a786916be46e50f169a1c70957bd
NafiurSiddiqui <saejim91@gmail.com>
2024-09-08 20:46:34 +0600

    UI : scifi btn light mode applied

--- resources/views/components/form/sci-fi-btn.blade.php
+++ resources/views/components/form/sci-fi-btn.blade.php
@@ -9,13 +9,13 @@
             <path
                 d="M1 22.9072V48C1 48.5523 1.44772 49 2 49H121.15C121.496 49 121.817 48.8213 121.999 48.5274L132.85 31.05C132.948 30.8916 133 30.7089 133 30.5225V7C133 6.44772 132.552 6 132 6H15.5788C15.2862 6 15.0082 6.12818 14.8182 6.35075L1.23942 22.258C1.08489 22.439 1 22.6692 1 22.9072Z"
                 fill="#181B1A" stroke="#474E4B"
-                class="group-hover:fill-logoColor transition-colors duration-100 ease-in" />
+                class="group-hover:fill-logoColor group-hover:stroke-emerald-500 transition-colors duration-100 ease-in fill-lightPage dark:fill-[#181B1A]" />
             <path
                 d="M1 17.9072V43C1 43.5523 1.44772 44 2 44H121.15C121.496 44 121.817 43.8213 121.999 43.5274L132.85 26.05C132.948 25.8916 133 25.7089 133 25.5225V2C133 1.44772 132.552 1 132 1H15.5788C15.2862 1 15.0082 1.12818 14.8182 1.35075L1.23942 17.258C1.08489 17.439 1 17.6692 1 17.9072Z"
                 fill="#181B1A" stroke="#474E4B"
-                class="group-hover:fill-logoColor transition-colors duration-100 ease-in" />
-            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#14E785"
-                class="group-hover:fill-[#0C0C0C] group-active:fill-[#0C0C0C] group-focus:fill-[#0C0C0C] "
+                class="scifi-btn-top-layer  group-hover:fill-logoColor group-hover:stroke-emerald-500 transition-colors duration-100 ease-in fill-lightPage dark:fill-[#181B1A]" />
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"
+                class="group-hover:fill-[#225D42] dark:group-hover:fill-[#0C0C0C] group-active:fill-[#0C0C0C] group-focus:fill-[#0C0C0C] fill-emerald-700 dark:fill-[#14E785]"
                 font-size="16px" font-family="Arial, sans-serif">
                 {{ $label }}
             </text>
@@ -30,13 +30,13 @@
             <path
                 d="M1 22.9072V48C1 48.5523 1.44772 49 2 49H121.15C121.496 49 121.817 48.8213 121.999 48.5274L132.85 31.05C132.948 30.8916 133 30.7089 133 30.5225V7C133 6.44772 132.552 6 132 6H15.5788C15.2862 6 15.0082 6.12818 14.8182 6.35075L1.23942 22.258C1.08489 22.439 1 22.6692 1 22.9072Z"
                 fill="#181B1A" stroke="#474E4B"
-                class="group-hover:fill-darkTextHover-600 transition-colors duration-100 ease-in" />
+                class="group-hover:fill-darkTextHover-600 group-hover:stroke-emerald-500 transition-colors duration-100 ease-in fill-lightPage dark:fill-[#181B1A]" />
             <path
                 d="M1 17.9072V43C1 43.5523 1.44772 44 2 44H121.15C121.496 44 121.817 43.8213 121.999 43.5274L132.85 26.05C132.948 25.8916 133 25.7089 133 25.5225V2C133 1.44772 132.552 1 132 1H15.5788C15.2862 1 15.0082 1.12818 14.8182 1.35075L1.23942 17.258C1.08489 17.439 1 17.6692 1 17.9072Z"
                 fill="#181B1A" stroke="#474E4B"
-                class="group-hover:fill-darkTextHover-600 transition-colors duration-100 ease-in" />
-            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#14E785"
-                class="group-hover:fill-[#0C0C0C] group-active:fill-[#0C0C0C] group-focus:fill-[#0C0C0C] "
+                class="group-hover:fill-darkTextHover-600 group-hover:stroke-emerald-500 transition-colors duration-100 ease-in fill-lightPage dark:fill-[#181B1A]" />
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"
+                class="group-hover:fill-[#225D42] dark:group-hover:fill-[#0C0C0C] group-active:fill-[#0C0C0C] group-focus:fill-[#0C0C0C] fill-emerald-700  dark:fill-[#14E785]"
                 font-size="16px" font-family="Arial, sans-serif">
                 {{ $label }}
             </text>
