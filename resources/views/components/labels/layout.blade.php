78d5d6c381a19848caa24321123865740cd03434
NafiurSiddiqui <saejim91@gmail.com>
2024-09-11 02:21:00 +0600

    No hover and dakrmode support

--- resources/views/components/labels/layout.blade.php
+++ resources/views/components/labels/layout.blade.php
@@ -2,7 +2,7 @@
 
 @if (isset($noHref) && $noHref)
     <div
-        class="inline-block bg-[#DEEDE8] hover:bg-[#d5fff1] dark:text-darkText-100 text-center text-emerald-700 font-semibold {{ $sm ? 'text-xs px-2' : 'text-sm px-4' }}  py-1 min-w-4">
+        class="inline-block bg-[#DEEDE8] dark:bg-darkLabel dark:text-darkText-100 text-center text-emerald-700 font-semibold {{ $sm ? 'text-xs px-2' : 'text-sm px-4' }}  py-1 min-w-4">
         {{ $slot }}
     </div>
 @else
