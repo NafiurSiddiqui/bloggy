cb695603fdfa3af0a84d12fdfaa19fdff31fe3b5
NafiurSiddiqui <saejim91@gmail.com>
2024-09-17 22:06:12 +0600

    UI (component): toast info created

---
+++ resources/views/components/toast/info.blade.php
@@ -0,0 +1,6 @@
+    <x-panel class="flex justify-between items-center">
+        <div>
+            {{ $slot }}
+        </div>
+        <i class="fa-solid fa-circle-info text-xl text-lightText-300"></i>
+    </x-panel>
