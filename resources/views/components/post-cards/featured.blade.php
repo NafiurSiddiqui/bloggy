6cb071533ae3b871e3f3dcb1902802ba2be61b94
NafiurSiddiqui <saejim91@gmail.com>
2024-09-18 21:51:28 +0600

    Guard added

--- resources/views/components/post-cards/featured.blade.php
+++ resources/views/components/post-cards/featured.blade.php
@@ -4,7 +4,7 @@
 
 <x-post-cards.layout class="!p-0 w-full">
     <div class="flex relative flex-col md:flex-row bg-center bg-cover h-[400px] lg:h-[600px] bg-no-repeat"
-        style="background-image: url({{ $post->getFirstMedia('thumbnails')->getUrl() }})">
+        style="background-image: url({{ $post->getFirstMedia('thumbnails')?->getUrl() }})">
         <x-post-cards.featured-overlay class="h-full" />
         <div class="flex flex-wrap items-baseline justify-end space-y-1 space-x-4 absolute right-4 top-4">
             <x-labels.category :category="$post->category" />
